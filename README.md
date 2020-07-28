d2e48bcf6c46f3793577339b700fe25e3d57ddbd
hybridpollo <hybridpollo@gmail.com>
2020-07-28 16:36:51 -0500

    Updated README.md and included OSP16 TLS-Everywhere Templates.

--- README.md
+++ README.md
@@ -1,18 +1,25 @@
-#### About this Repository
-This repository contains a repository of templates that have been used to test 
-some of the newer features in OSP 16. Each top level directory represent
-their individual set of templates to support the deployment scenario.
+## Redhat OpenStack Platform Deployment Templates
 
-These are some of the interesting deployment scenarios covered by these
-templates:
+#### About this Repository
+This repository contains a collection of Tripleo Heat templates 
+used to test a few of the new features in Red Hat OpenStack Platform 16, 
+in addition to serving as a collection of other general purpose deployment templates. 
+Each top level directory represent a deployment template for a desired used case. 
+Additional branches on this repository will also have a collection of templates for Red Hat 
+OpenStack Platform 13.
+
+These are some of the interesting deployment scenarios covered by these templates:
 - Deploying Multiple Overclouds from a Single Overcloud
 - Splitting the Overcloud deployment by using Multiple Stacks
 
 Additional information for each deployment template set can be found in the
 README.md for each deployment scenario.
 
-
-
 #### Important Notes
-These templates were tested on RHOSP 16.0.2 where this Undercloud feature is offered as a tech preview, however full support
-for multi-stack and multi overcloud deployments are expected on RHOSP 16.1.
+These templates were tested on RHOSP 16.0.2 where this Undercloud feature is offered as a tech   
+preview, however full support for multi-stack and multi overcloud deployments are expected on RHOSP 16.1.
+Features such as predictable IP addressing, hostnames, and TLS-Everywhere have been fully supported since RHOSP 13.
+
+#### Documentation
+- [Red Hat OpenStack Platform Documentation](https://access.redhat.com/documentation/en-us/red_hat_openstack_platform)
+- [Tripleo Upstream Documentation](https://docs.openstack.org/tripleo-docs/latest/)
---
+++ osp16_tls_everywhere_overcloud/deploy_commands/overcloud_deploy_tls_everywhere.sh
@@ -0,0 +1,52 @@
+#!/usr/bin/env bash 
+#
+###################################################                                                               
+# Variables
+###################################################                                              
+THT_DEFAULT="/usr/share/openstack-tripleo-heat-templates"
+THT_CUSTOM_TEMPLATES="/home/stack/overcloud_deployment/templates"
+RC_FILE="/home/stack/stackrc"
+
+if [[ $USER != "stack" ]]; then
+  echo -e "ERROR: Deploy script  must be run as the stack user."
+  exit 1
+fi 
+
+echo 'Running the overcloud deployment.'
+
+if test -f "${RC_FILE}" ; then  
+  source ${RC_FILE}
+  cd /home/stack
+else
+  echo -e "Undercloud environment file: ${RC_FILE} does not exist!"
+  exit 1
+fi
+
+
+###################################################                                                               
+# Deploy 
+###################################################    
+
+openstack overcloud deploy --templates \
+  -r ${THT_CUSTOM_TEMPLATES}/roles_data.yaml \
+  -n ${THT_CUSTOM_TEMPLATES}/network_data.yaml \
+  -e ${THT_DEFAULT}/environments/network-isolation.yaml \
+  -e ${THT_DEFAULT}/environments/services/neutron-ovn-dvr-ha.yaml \
+  -e ${THT_DEFAULT}/environments/ssl/enable-internal-tls.yaml \
+  -e ${THT_DEFAULT}/environments/ssl/tls-everywhere-endpoints-dns.yaml \
+  -e ${THT_CUSTOM_TEMPLATES}/container_image_prepare.yaml \
+  -e ${THT_CUSTOM_TEMPLATES}/node_info.yaml \
+  -e ${THT_CUSTOM_TEMPLATES}/customizations.yaml \
+  -e ${THT_CUSTOM_TEMPLATES}/first_boot.yaml \
+  -e ${THT_CUSTOM_TEMPLATES}/environments/network_environment.yaml \
+  -e ${THT_CUSTOM_TEMPLATES}/predictable_ip_addresses.yaml \
+  -e ${THT_CUSTOM_TEMPLATES}/predictable_hostnames.yaml \
+  -e ${THT_CUSTOM_TEMPLATES}/environments/tls_endpoints.yaml \
+  -e ${THT_CUSTOM_TEMPLATES}/environments/enable_tls.yaml \
+  -e ${THT_CUSTOM_TEMPLATES}/environments/glance_nfs.yaml \
+  -e ${THT_CUSTOM_TEMPLATES}/environments/cinder_nfs.yaml \
+  -e ${THT_CUSTOM_TEMPLATES}/environments/ssh_banner.yaml \
+  -e ${THT_CUSTOM_TEMPLATES}/environments/disable_telemetry.yaml \
+  -e ${THT_CUSTOM_TEMPLATES}/environments/disable_panko.yaml \
+  -e ${THT_CUSTOM_TEMPLATES}/environments/disable_swift.yaml \
+  -e ${THT_CUSTOM_TEMPLATES}/satellite_registration.yaml 
